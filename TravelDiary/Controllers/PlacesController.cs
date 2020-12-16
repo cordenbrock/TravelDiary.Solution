933e6b6f1d2bc966f2064f9b98e8a62c5bf38b80
cordenbrock <cordenbrock@gmail.com>
2020-12-16 17:28:33 -0500

    Ensure MVC functionality
    Co-authored-by: Max Brockbank <maxbrockbank1999@gmail.com>

--- TravelDiary/Controllers/PlacesController.cs
+++ TravelDiary/Controllers/PlacesController.cs
@@ -1,11 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
-
+using TravelDiary.Models;
+using System.Collections.Generic;
 namespace TravelDiary.Controllers 
 {
   
   public class PlacesController : Controller
   {
+    [HttpGet("/places")]
+    public ActionResult Index()
+    {
+      List<Place> allPlaces = Place.GetAll();
+      return View(allPlaces);
+    }
 
+    [HttpGet("/places/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost ("/places")]
+    public ActionResult Create(string place, string description)
+    {
+      Place newPlace = new Place(place, description);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/places/{id}")]
+    public ActionResult Show(int id)
+    {
+        Place place = Place.Find(id);
+        return View(place);
+    }
   }
 
 }
--- TravelDiary/Models/Places.cs
+++ TravelDiary/Models/Places.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
-
 namespace TravelDiary.Models
 {
   public class Place
   {
     public string CityName {get;set;}
+    public string Description {get;set;}
+
+    // public Image Photo {get;set;}
+    // public Image image {get;set;}
     public int Id {get;}
     public static List<Place> _instances = new List<Place>{};
     public Place(string cityName)
@@ -16,7 +19,20 @@ namespace TravelDiary.Models
 
     }
 
-    public static List<Place> GetAll(){
+    public Place(string cityName, string description)
+    : this(cityName)
+    {
+      Description = description;
+
+    }
+    // public Place(string cityName, string description, Image photo)
+    // : this(cityName, description)
+    // {
+    //   Photo = photo;
+    // }
+
+    public static List<Place> GetAll()
+    {
       return _instances;
     }
     public static Place Find(int searchId)
